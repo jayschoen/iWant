818538d057b9e598466faf07a67133deaa92b30d
Jay Schoen <jay.r.schoen@gmail.com>
2020-05-28 12:22:08 -0400

    move controllers; add helpers; refactor resources

--- Go/cmd/server/main.go
+++ Go/cmd/server/main.go
@@ -1,54 +1,155 @@
 package main
 
-import(
+import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	// "fmt"
-	"encoding/json"
 	"strconv"
 
 	"github.com/gorilla/mux"
 
-	controllers "github.com/jayschoen/iWant-slack-bot/internal"
+	controllers "github.com/jayschoen/iWant-slack-bot/controllers"
+	helpers "github.com/jayschoen/iWant-slack-bot/helpers"
 )
 
 func get(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
-	id, hasId := vars["id"]
+	paramID, hasID := vars["id"]
 
-	if !hasId {
+	if !hasID {
 
-		json.NewEncoder(w).Encode(controllers.GetAllWants())
-
-	} else {
-
-		id, err := strconv.Atoi(id)
-		if err != nil || id < 1 {
-			panic( err.Error() )
+		wants, err := controllers.GetAllWants()
+		if err != nil {
+			helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
+			return
 		}
 
-		json.NewEncoder(w).Encode(controllers.GetWantById(id))
+		helpers.RespondWithJSON(w, http.StatusOK, wants)
+		return
 
 	}
+
+	id, err := strconv.Atoi(paramID)
+	if err != nil || id < 1 {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid Want ID")
+		return
+	}
+
+	want, err := controllers.GetWantByID(id)
+	if err != nil {
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	helpers.RespondWithJSON(w, http.StatusOK, want)
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write( []byte( `{"message": "POST"}` ) )
+
+	vars := mux.Vars(r)
+	params := r.URL.Query()
+
+	paramID, hasID := vars["id"]
+
+	if !hasID {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Missing Want ID")
+		return
+	}
+
+	id, err := strconv.Atoi(paramID)
+	if err != nil || id < 1 {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid Want ID")
+		return
+	}
+
+	if len(params) == 0 {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Parameters Are Required")
+		return
+	}
+
+	status, statusExists := params["status"]
+	wants, wantsExists := params["wants"]
+	targetTime, targetTimeExists := params["targetTime"]
+
+	if !statusExists && !wantsExists && !targetTimeExists {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Missing Parameter - Request Must Include At Least status Or wants Or targetTime")
+		return
+	}
+
+	if err := controllers.UpdateWant(id, status, wants, targetTime); err != nil {
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	helpers.RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+
 }
 
 func put(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write( []byte( `{"message": "PUT"}` ) )
+	params := r.URL.Query()
+
+	if len(params) == 0 {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Parameters Are Required")
+		return
+	}
+
+	paramSlackID, slackIDExists := params["slackID"]
+	status, statusExists := params["status"]
+	wants, wantsExists := params["wants"]
+	targetTime, targetTimeExists := params["targetTime"]
+
+	exists := make(map[string]bool)
+	exists["slackID"] = slackIDExists
+	exists["status"] = statusExists
+	exists["wants"] = wantsExists
+	exists["targetTime"] = targetTimeExists
+
+	for paramName, exist := range exists {
+		if !exist {
+			helpers.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Missing Parameter: %s", paramName))
+			return
+		}
+	}
+
+	slackID, err := strconv.Atoi(paramSlackID[0])
+	if err != nil || slackID < 1 {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid Slack ID")
+		return
+	}
+
+	if err := controllers.InsertWant(slackID, status[0], wants[0], helpers.ParseTimeString(targetTime[0])); err != nil {
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	helpers.RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func delete(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write( []byte( `{"message": "DELETE"}` ) )
+
+	vars := mux.Vars(r)
+	paramID, hasID := vars["id"]
+
+	if !hasID {
+
+		helpers.RespondWithError(w, http.StatusBadRequest, "Missing Want ID")
+		return
+
+	}
+
+	id, err := strconv.Atoi(paramID)
+	if err != nil || id < 1 {
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid Want ID")
+		return
+	}
+
+	if err := controllers.DeleteWant(id); err != nil {
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	helpers.RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 /*
@@ -71,14 +172,18 @@ func main() {
 
 	r := mux.NewRouter().StrictSlash(true)
 
-	r.HandleFunc("/get-wants/", get)
+	r.HandleFunc("/get-wants", get)
 	r.HandleFunc("/get-wants/{id}", get)
-	r.HandleFunc("/create-want", post)
-	r.HandleFunc("/update-want/{id}", put)
+
+	r.HandleFunc("/create-want", put)
+
+	r.HandleFunc("/update-want", post)
+	r.HandleFunc("/update-want/{id}", post)
+
+	r.HandleFunc("/delete-want", delete)
 	r.HandleFunc("/delete-want/{id}", delete)
-	// r.HandleFunc("/", notFound)
 
 	r.HandleFunc("/tests", executeTests)
 
-	log.Fatal(http.ListenAndServe(":8080", r) )
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
