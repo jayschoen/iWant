9eb5144c359ff8e6ca18b0912c5890e9661c2d81
Jay Schoen <jay.r.schoen@gmail.com>
2020-07-06 15:17:59 -0400

    update create table script to reflect schema changes

--- MySQL/iWant.sql
+++ MySQL/iWant.sql
@@ -1,10 +1,12 @@
 Use iWant_db;
 
 Create table `whatsup` (
-	slack_id int,
+	id int NOT NULL AUTO_INCREMENT,
+	slackID int NOT NULL,
 	status varchar(20),
 	wants varchar(50),
-	created datetime,
-	target_time datetime
+	created datetime NOT NULL,
+	targetTime datetime,
+	PRIMARY KEY (id)
 );
 
